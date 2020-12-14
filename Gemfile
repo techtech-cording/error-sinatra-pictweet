68a9366bddbef8a81f28fd602ca1615c0c372f66
EC2 Default User <ec2-user@ip-172-31-21-187.us-east-2.compute.internal>
2020-12-14 06:42:36 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+gem 'sinatra'
+gem 'sinatra-reloader'
+gem 'sinatra-activerecord'
+gem 'activerecord', '~> 5.2.0'
+gem 'sqlite3'
+gem 'rake'
+gem 'pry'
---
+++ pictweet.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+set :database, {adapter: "sqlite3", database: "tweets.sqlite3"}
+
+class Tweet < ActiveRecord::Base
+end
+
+get "/" do
+  @tweets = Tweet.all
+  erb :index
+end
+
+get "/tweets/new" do
+  erb :new
+end
+
+post "/tweets" do
+  Tweet.create(params)
+  redirect '/'
+end
