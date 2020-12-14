a204e03ac27aa80ef8d169a5a064e1e53afe2fa5
EC2 Default User <ec2-user@ip-172-31-21-187.us-east-2.compute.internal>
2020-12-14 07:42:10 +0000

    add edit delete func

--- pictweet.rb
+++ pictweet.rb
@@ -21,3 +21,25 @@ post "/tweets" do
   Tweet.create(params)
   redirect '/'
 end
+
+get "/tweets/:id/edit" do
+  @tweet = Tweet.find(params[:id])
+  erb :edit
+end
+
+patch "/tweets/:id" do
+  tweet = Tweet.find(params[:id])
+  tweet.update(text: params[:text], image: params[:image])
+  redirect "/"
+end
+
+get "/tweets/:id/delete" do
+  @tweet = Tweet.find(params[:id])
+  erb :delete
+end
+
+delete "/tweets/:id" do
+  tweet = Tweet.find(params[:id])
+  tweet.delete
+  redirect "/"
+end
