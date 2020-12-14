36530bf5d30eadb1048526b2772a5e568eccc4f3
EC2 Default User <ec2-user@ip-172-31-21-187.us-east-2.compute.internal>
2020-12-14 11:57:20 +0000

    edit github link

--- error.sh
+++ error.sh
@@ -2,6 +2,6 @@
 
 cd ../
 rm -rf error-sinatra-pictweet
-git clone -b $1 https://github.com/Sasaki1994/error-sinatra-pictweet.git
+git clone -b $1 https://github.com/techtech-cording/error-sinatra-pictweet.git
 cd error-sinatra-pictweet
 bundle update
